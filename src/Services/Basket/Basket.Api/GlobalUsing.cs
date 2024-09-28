61ecb86160b499421d13ca4d5ea972d85af578e6
AKUBUE ALEXANDER <info.dilistech@gmail.com>
2024-09-28 08:55:44 +0100

    Basket Api updated

---
+++ src/Services/Basket/Basket.Api/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using Basket.Api.Models;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlocks.Behaviours;
+global using Marten;
+global using Basket.Api.Data;
+global using Basket.Api.Exceptions;
+
---
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -0,0 +1,33 @@
+using Basket.Api.Data;
+using Basket.API.Data;
+using BuildingBlocks.Exceptions.Handler;
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapCarter();
+
+app.Run();
